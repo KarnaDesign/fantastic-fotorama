b7465ca958ecdd1eba7a0e2a08410e0d8951e1ad
Ida Ferm <ida.kg.ferm@gmail.com>
2016-01-13 13:25:05 +0100

    Added a bit of design

--- index.php
+++ index.php
@@ -4,8 +4,32 @@
     <title>Fantastic Fotorama</title>
     <link rel="stylesheet" href="style/style.css">
     <script src="libs/jquery/jquery.js"></script>
+    <link href="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.css" rel="stylesheet">
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.js"></script>
+    <link href='https://fonts.googleapis.com/css?family=Aguafina+Script' rel='stylesheet' type='text/css'>
+    <link href="libs/fotorama/fotorama.css" rel="stylesheet">
+    <script src="libs/fotorama/fotorama.js"></script>
 </head>
 <body>
+
+<header><h1>Fantastic Fotorama</h1></header>
+
+<div id="wrapper">
+    <div class="fotorama">
+        <img src="images/autumn-985918_1920.jpg">
+        <img src="images/autumn-1072827_1920.jpg">
+        <img src="images/dandelion-437827_1920.jpg">
+        <img src="images/hirsch-643340_1920.jpg">
+        <img src="images/lake-696098_1920.jpg">
+        <img src="images/landscape-1034077_1920.jpg">
+        <img src="images/nature-sunset-984560_1920.jpg">
+        <img src="images/seagulls-815304_1920.jpg">
+        <img src="images/sleigh-ride-549727_1920.jpg">
+        <img src="images/sparrow-50346_1920.jpg">
+        <img src="images/swan-319379_1920.jpg">
+        <img src="images/tulips-56423_1920.jpg">
+    </div>
+</div>
 
 </body>
 
