12deb871fc0d8a6374b72afa6496d920caf78452
Ida Ferm <ida.kg.ferm@gmail.com>
2016-01-14 16:21:07 +0100

    Added the smaller images

--- index.php
+++ index.php
@@ -16,19 +16,18 @@
 
 <div id="wrapper">
     <div class="fotorama" data-nav="thumbs">
-        
-        <img src="images/autumn-985918_1920.jpg">
-        <img src="images/autumn-1072827_1920.jpg">
-        <img src="images/dandelion-437827_1920.jpg">
-        <img src="images/hirsch-643340_1920.jpg">
-        <img src="images/lake-696098_1920.jpg">
-        <img src="images/landscape-1034077_1920.jpg">
-        <img src="images/nature-sunset-984560_1920.jpg">
-        <img src="images/seagulls-815304_1920.jpg">
-        <img src="images/sleigh-ride-549727_1920.jpg">
-        <img src="images/sparrow-50346_1920.jpg">
-        <img src="images/swan-319379_1920.jpg">
-        <img src="images/tulips-56423_1920.jpg">
+        <a href="images/autumn-985918_1920.jpg"><img src="images/autumn-985918_1920_thumb.jpg"></a>
+        <a href="images/autumn-1072827_1920.jpg"><img src="images/autumn-1072827_1920_thumb.jpg"></a>
+        <a href="images/dandelion-437827_1920.jpg"><img src="images/dandelion-437827_1920_thumb.jpg"></a>
+        <a href="images/hirsch-643340_1920.jpg"><img src="images/hirsch-643340_1920_thumb.jpg"></a>
+        <a href="images/lake-696098_1920.jpg"><img src="images/lake-696098_1920_thumb.jpg"></a>
+        <a href="images/landscape-1034077_1920.jpg"><img src="images/landscape-1034077_1920_thumb.jpg"></a>
+        <a href="images/nature-sunset-984560_1920.jpg"><img src="images/nature-sunset-984560_1920_thumb.jpg"></a>
+        <a href="images/seagulls-815304_1920.jpg"><img src="images/seagulls-815304_1920_thumb.jpg"></a>
+        <a href="images/sleigh-ride-549727_1920.jpg"><img src="images/sleigh-ride-549727_1920_thumb.jpg"></a>
+        <a href="images/sparrow-50346_1920.jpg"><img src="images/sparrow-50346_1920_thumb.jpg"></a>
+        <a href="images/swan-319379_1920.jpg"><img src="images/swan-319379_1920_thumb.jpg"></a>
+        <a href="images/tulips-56423_1920.jpg"><img src="images/tulips-56423_1920_thumb.jpg"></a>
     </div>
 </div>
 
