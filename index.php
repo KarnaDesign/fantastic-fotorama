ec1d9fda5b358c4d27c5e378dc35600573086367
Ida Ferm <ida.kg.ferm@gmail.com>
2016-01-15 09:40:46 +0100

    Added captions using data-caption

--- index.php
+++ index.php
@@ -16,18 +16,18 @@
 
 <div id="wrapper">
     <div class="fotorama" data-nav="thumbs">
-        <a href="images/autumn-985918_1920.jpg"><img src="images/autumn-985918_1920_thumb.jpg"></a>
-        <a href="images/autumn-1072827_1920.jpg"><img src="images/autumn-1072827_1920_thumb.jpg"></a>
-        <a href="images/dandelion-437827_1920.jpg"><img src="images/dandelion-437827_1920_thumb.jpg"></a>
-        <a href="images/hirsch-643340_1920.jpg"><img src="images/hirsch-643340_1920_thumb.jpg"></a>
-        <a href="images/lake-696098_1920.jpg"><img src="images/lake-696098_1920_thumb.jpg"></a>
-        <a href="images/landscape-1034077_1920.jpg"><img src="images/landscape-1034077_1920_thumb.jpg"></a>
-        <a href="images/nature-sunset-984560_1920.jpg"><img src="images/nature-sunset-984560_1920_thumb.jpg"></a>
-        <a href="images/seagulls-815304_1920.jpg"><img src="images/seagulls-815304_1920_thumb.jpg"></a>
-        <a href="images/sleigh-ride-549727_1920.jpg"><img src="images/sleigh-ride-549727_1920_thumb.jpg"></a>
-        <a href="images/sparrow-50346_1920.jpg"><img src="images/sparrow-50346_1920_thumb.jpg"></a>
-        <a href="images/swan-319379_1920.jpg"><img src="images/swan-319379_1920_thumb.jpg"></a>
-        <a href="images/tulips-56423_1920.jpg"><img src="images/tulips-56423_1920_thumb.jpg"></a>
+        <a href="images/autumn-985918_1920.jpg" data-caption="Early autumn landscape"><img src="images/autumn-985918_1920_thumb.jpg"></a>
+        <a href="images/autumn-1072827_1920.jpg" data-caption="Autumn landscape"><img src="images/autumn-1072827_1920_thumb.jpg"></a>
+        <a href="images/dandelion-437827_1920.jpg" data-caption="Dandelion"><img src="images/dandelion-437827_1920_thumb.jpg"></a>
+        <a href="images/hirsch-643340_1920.jpg" data-caption="Hirsch"><img src="images/hirsch-643340_1920_thumb.jpg"></a>
+        <a href="images/lake-696098_1920.jpg" data-caption="Lake in sunset"><img src="images/lake-696098_1920_thumb.jpg"></a>
+        <a href="images/landscape-1034077_1920.jpg" data-caption="Rolling hills"><img src="images/landscape-1034077_1920_thumb.jpg"></a>
+        <a href="images/nature-sunset-984560_1920.jpg" data-caption="Sunset on water"><img src="images/nature-sunset-984560_1920_thumb.jpg"></a>
+        <a href="images/seagulls-815304_1920.jpg" data-caption="Seagulls"><img src="images/seagulls-815304_1920_thumb.jpg"></a>
+        <a href="images/sleigh-ride-549727_1920.jpg" data-caption="Sleigh-ride"><img src="images/sleigh-ride-549727_1920_thumb.jpg"></a>
+        <a href="images/sparrow-50346_1920.jpg" data-caption="Sparrow"><img src="images/sparrow-50346_1920_thumb.jpg"></a>
+        <a href="images/swan-319379_1920.jpg" data-caption="Swan"><img src="images/swan-319379_1920_thumb.jpg"></a>
+        <a href="images/tulips-56423_1920.jpg" data-caption="Tulips"><img src="images/tulips-56423_1920_thumb.jpg"</a>
     </div>
 </div>
 
